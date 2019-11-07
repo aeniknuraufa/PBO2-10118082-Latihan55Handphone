0e655bfa32a584350f13bb9c5017d745b57df66e
Aenik Nur Aufa <aeniknuraufa@users.noreply.github.com>
2019-11-07 22:46:20 +0700

    PBO2-10118082-Latihan55
    Membuat program berbasis object untuk mendapatkan informasi handphone

---
+++ src/pbo2/pkg10118082/latihan55/handphone/BlackBerry.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118082.latihan55.handphone;
+
+/**
+ *	
+ * @author
+ * Nama     : Aenik Nur Aufa
+ * Kelas    : IF 2
+ * NIM      : 10118082
+ * Deskripsi Program : Membuat program berbasis object untuk mendapatkan
+ * informasi handphone
+ */
+public class BlackBerry extends Handphone {
+    private String pinBB;
+
+    public BlackBerry(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+    
+    public String getPinBB() {
+        return pinBB;
+    }
+
+    public void setPinBB(String pinBB) {
+        this.pinBB = pinBB;
+    }
+    
+}
---
+++ src/pbo2/pkg10118082/latihan55/handphone/WindowsPhone.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118082.latihan55.handphone;
+
+/**
+ *	
+ * @author
+ * Nama     : Aenik Nur Aufa
+ * Kelas    : IF 2
+ * NIM      : 10118082
+ * Deskripsi Program : Membuat program berbasis object untuk mendapatkan
+ * informasi handphone
+ */
+public class WindowsPhone extends Handphone {
+    private String wpKeyStore;
+
+    public WindowsPhone(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+     
+    public String getWpKeyStore() {
+        return wpKeyStore;
+    }
+
+    public void setWpKeyStore(String wpKeyStore) {
+        this.wpKeyStore = wpKeyStore;
+    }
+     
+}
